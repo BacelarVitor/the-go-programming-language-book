6e24ac708b366d22a09968d82caebe09e82110fd
Vitor Bacelar de Paula Augusto <vitor.bacelar@protonmail.com>
2023-10-03 17:45:15 -0300

    Exercise 1.2

--- main.go
+++ main.go
@@ -6,9 +6,15 @@ import (
 )
 
 func main() {
+	/* Exercise 1.1.
 	var arg string
 	for _, s := range os.Args {
 		arg += s + " "
 	}
 	fmt.Println(arg)
+	*/
+
+	for i, s := range os.Args {
+		fmt.Printf("%d - %s\n", i, s)
+	}
 }
