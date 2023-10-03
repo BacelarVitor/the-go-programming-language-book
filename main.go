4384198a2a065e8e975246161202fba127073282
Vitor Bacelar de Paula Augusto <vitor.bacelar@protonmail.com>
2023-10-03 17:53:37 -0300

    exercise 1.3

--- main.go
+++ main.go
@@ -1,10 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"os"
-)
-
 func main() {
 	/* Exercise 1.1.
 	var arg string
@@ -12,9 +7,26 @@ func main() {
 		arg += s + " "
 	}
 	fmt.Println(arg)
-	*/
-
+	* Exercise 1.2
 	for i, s := range os.Args {
 		fmt.Printf("%d - %s\n", i, s)
 	}
+	* Exercise 1.3
+	startJoin := time.Now()
+	fmt.Print(strings.Join(os.Args[1:], " "))
+	endJoin := time.Now()
+	executionJoin := endJoin.Sub(startJoin)
+
+	startFor := time.Now()
+	var arg string
+	for _, s := range os.Args {
+		arg += s + " "
+	}
+	fmt.Println(arg)
+	endFor := time.Now()
+	executionFor := endFor.Sub(startFor)
+
+	fmt.Println(executionJoin)
+	fmt.Println(executionFor)
+	*/
 }
