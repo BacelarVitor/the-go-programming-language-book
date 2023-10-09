80a198d0557dd8fa47029955030105d7dba75225
Vitor Bacelar de Paula Augusto <vitor.bacelar@protonmail.com>
2023-10-09 13:42:46 -0300

    create a gif using lissanjous technique

--- main.go
+++ main.go
@@ -1,32 +1,17 @@
 package main
 
+import (
+	"image/color"
+	"os"
+)
+
+var palette = []color.Color{color.White, color.Black}
+
+const (
+	whiteIndex = 0
+	blackIndex = 1
+)
+
 func main() {
-	/* Exercise 1.1.
-	var arg string
-	for _, s := range os.Args {
-		arg += s + " "
-	}
-	fmt.Println(arg)
-	* Exercise 1.2
-	for i, s := range os.Args {
-		fmt.Printf("%d - %s\n", i, s)
-	}
-	* Exercise 1.3
-	startJoin := time.Now()
-	fmt.Print(strings.Join(os.Args[1:], " "))
-	endJoin := time.Now()
-	executionJoin := endJoin.Sub(startJoin)
-
-	startFor := time.Now()
-	var arg string
-	for _, s := range os.Args {
-		arg += s + " "
-	}
-	fmt.Println(arg)
-	endFor := time.Now()
-	executionFor := endFor.Sub(startFor)
-
-	fmt.Println(executionJoin)
-	fmt.Println(executionFor)
-	*/
+	lissajous(os.Stdout)
 }
