ca6be285901bd0023e8c3640424957e39c776011
Vitor Bacelar de Paula Augusto <vitor.bacelar@protonmail.com>
2023-10-09 16:41:11 -0300

    create a fetch

---
+++ fetch.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func fetch(url string) {
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+		os.Exit(1)
+	}
+	b, err := io.ReadAll(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+		os.Exit(1)
+	}
+	fmt.Printf("%s", b)
+}
--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+import "os"
+
 func main() {
 	// lissajous(os.Stdout)
+	for _, url := range os.Args[1:] {
+		fetch(url)
+	}
 }
