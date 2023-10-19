48deb6c41cc6869c9ab4b2effc62b780ab7eba31
Vitor Bacelar de Paula Augusto <vitor.bacelar@protonmail.com>
2023-10-19 13:51:34 -0300

    tried and failed to add new content to the file

--- fetchall.go
+++ fetchall.go
@@ -10,19 +10,26 @@ import (
 	"time"
 )
 
+const filePath = "featchingData.txt"
+
 func fetchall(urls []string) {
 	flag.CommandLine.Set("align", "10")
 	start := time.Now()
 	ch := make(chan string)
-	fmt.Printf("\n###### %s ######\n\n", start.Format(time.RFC822))
-	fmt.Println("time\t bytes\t url\n")
+
+	var fileContent string
+	fileContent += fmt.Sprintf("\n###### %s ######\n\n", start.Format(time.RFC822))
+	fileContent += fmt.Sprintln("time\t bytes\t url\n")
+
 	for _, url := range urls {
 		go fetchGo(url, ch)
 	}
 	for range urls {
-		fmt.Println(<-ch)
+		fileContent += fmt.Sprintln(<-ch)
 	}
-	fmt.Printf("\n%.2fs\t elapsed\n", time.Since(start).Seconds())
+	fileContent += fmt.Sprintf("\n%.2fs\t elapsed\n", time.Since(start).Seconds())
+
+	writeFile(fileContent)
 }
 
 func fetchGo(url string, ch chan<- string) {
@@ -56,4 +63,51 @@ func getUrls() []string {
 		}
 	}
 	return urls
+}
+
+func writeFile(writableContent string) {
+	file, err := os.OpenFile(filePath, os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	_, err = io.WriteString(file, writableContent)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	err = file.Sync()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	buffer := make([]byte, 1024)
+	n, err := file.Read(buffer)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	_, err = file.Seek(0, 0)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	_, err = file.Write([]byte(writableContent))
+	if err != nil {
+		// Handle the error.
+		fmt.Println(err)
+		return
+	}
+
+	_, err = file.Write(buffer[:n])
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
 }
